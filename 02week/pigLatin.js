31b05d4d0e6b59d85d0f7a3b08fdb5657fb3c19b
Almira Tan <atan512@yahoo.com>
2018-05-03 20:27:31 -0500

    uploading pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,14 +10,36 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
 
-  // Your code here
+  //get input/word from user
+  //create variable to reformat word
+  //create array in order to cycle through
+  //from the input, check to see if first letter is consonant or vowel
+  //use if statement - if vowel add "yay" to the end
+  // if consonant, use an array to cycle through until you get to a vowel (for loop function)
+  //take those letters that you cycled through and add them to the end using .split and .join methods
+  //use .trim method or .toLower method to clean up user's input
+  // shift method is for first letter, pop is for last letter
 
+  const cleanedUpInput = word.toLower().trim(); //take user's input and remove spaces and make it all lowercase
+  const letterArray = [cleanedUpInput.split()]; //turn the cleaned up input into an array so we can check the letters
+  const vowels = ["a", "e", "i", "o", "u"];
+  if (letterArray[0].includes(vowels)) { //check to see if first letter is a vowel
+    let formattedStr = letterArray + "yay";
+    console.log(formattedStr);
+  } else if (!letterArray[0].includes(vowels))
+    // const lastIndex = letterArray.length - 1;
+    // for (let index = lastIndex;)
+
+    const moveLetter = letterArray[0];
+  const rearrange = formattedStr.shift(); //remove first letter
+  const newWord = rearrange + moveLetter; //add removed letter to the end
+}
 }
 
 
 function getPrompt() {
   rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
+    console.log(pigLatin(answer));
     getPrompt();
   });
 }
